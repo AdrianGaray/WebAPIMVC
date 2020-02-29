29579b7641ef788bd1f2e01d9294bec0519b1794
adrian <adriangaray396@gmail.com>
2020-02-29 00:09:06 -0300

    Se agrega Contrlador Producto y una clase Producto. En el controller tiene dos metodos. Un metodo que devuelve todo el listado. Y el segundo metodo devulve un solo producto pasandole como parametro el ID del producto solicitado.

---
+++ WebAPIMVC/Controllers/ProductosController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIMVC.Models;
+
+namespace WebAPIMVC.Controllers
+{
+    public class ProductosController : ApiController
+    {
+        Productos[] productos = new Productos[]
+        {
+            new Productos{ ID=1, Nombre = "Producto 1", Categoria="Categoria 1", Precio =34 },
+            new Productos{ ID=2, Nombre = "Producto 2", Categoria="Categoria 2", Precio =35},
+            new Productos{ ID=3, Nombre = "Producto 3", Categoria="Categoria 3", Precio =36 }
+        };
+
+        public IEnumerable<Productos> GetTodosLosProductos()
+        {
+            return productos;
+
+        }
+
+        public IHttpActionResult GetProductos(int id)
+        {
+            var producto = productos.FirstOrDefault((p) => p.ID == id);
+
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            return Ok(producto);
+        }
+    }
+}
---
+++ WebAPIMVC/Models/Productos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIMVC.Models
+{
+    public class Productos
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Categoria { get; set; }
+        public int Precio { get; set; }
+    }
+}
